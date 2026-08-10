e4b35f3ed14c6212a26d782e4d6a34cd1f83084e
TOORU-321 <l.mineservice321@gmail.com>
2026-08-10 10:01:06 +0900

    config: ADMIN_TOKEN反映

--- kayomama-admin/config.js
+++ kayomama-admin/config.js
@@ -6,7 +6,7 @@
 window.GAS_URL = 'https://script.google.com/macros/s/AKfycbwyy_kIboj_EnyxbJSIhPFlUJOgITE1iSBMoHGLyoDyaMGkfbNd7PNaFf91xMnI-ZTb/exec';
 
 // 管理者トークン（GAS setupAdminSecrets() 実行時のログから取得して貼り付け）
-window.ADMIN_TOKEN = 'ここに-admin-xxxxx-を貼り付け';
+window.ADMIN_TOKEN = 'admin-c61fc1f6d1b44a828032';
 
 // Firebase Web SDK 設定（Firebase Console > プロジェクト設定 > 全般 > SDK設定）
 window.FIREBASE_CONFIG = {
